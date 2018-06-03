b1a9cb55df3c47181c5429ac7fd9db37764588e9
Lilly Dinh-le <lilly.dinhle@prodigygame.com>
2018-06-02 21:11:18 -0400

    Define a list of support instrument names

---
+++ src/lib/constants/INSTRUMENTS.js
@@ -0,0 +1,5 @@
+// For a full list of supported instruments, refer to:
+// https://github.com/danigb/soundfont-player/blob/master/instruments.json
+import INSTRUMENTS from 'soundfont-player/instruments.json';
+
+export default INSTRUMENTS;
